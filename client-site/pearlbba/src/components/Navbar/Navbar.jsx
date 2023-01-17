befc28af858003669ff5423643b0d93b7d6514f2
Anna M Attehed <anna.attehed@medieinstitutet.se>
2023-01-17 11:58:39 +0100

    icons added to navbar

--- client-site/pearlbba/src/components/Navbar/Navbar.jsx
+++ client-site/pearlbba/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import KeyboardDoubleArrowDownRoundedIcon from '@mui/icons-material/KeyboardDoubleArrowDownRounded';
+import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
+import ShoppingCartTwoToneIcon from '@mui/icons-material/ShoppingCartTwoTone';
+import PersonOutlineTwoToneIcon from '@mui/icons-material/PersonOutlineTwoTone';
+import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
 
 const Navbar = () => {
   return (
