edf460f5dd966265fe4de0422bdf4639a8bebcda
Anna M Attehed <anna.attehed@medieinstitutet.se>
2023-01-17 14:17:43 +0100

    navbar styling added

--- client-site/pearlbba/src/components/Navbar/Navbar.jsx
+++ client-site/pearlbba/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import KeyboardDoubleArrowDownRoundedIcon from '@mui/icons-material/KeyboardDoubleArrowDownRounded';
+import "./Navbar.scss";
 import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
 import ShoppingCartTwoToneIcon from '@mui/icons-material/ShoppingCartTwoTone';
 import PersonOutlineTwoToneIcon from '@mui/icons-material/PersonOutlineTwoTone';
@@ -9,43 +9,47 @@ import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className='wrapper-navbar'>
-        <div className='left-section'>
-            <div className="item">
+    <div className="navbar">
+      <div className="wrapper-navbar">
+        <div className="left-section">
+            <div className="section-links">
               <Link to="/products/1">Necklaces</Link>
             </div>
-            <div className="item">
+            <div className="section-links">
               <Link to="/products/2">Bracelets</Link>
             </div>
-            <div className="item">
+            <div className="section-links">
               <Link to="/products/3">Rings</Link>
             </div>
         </div>
 
-        <div className='center-section'>
+        <div className="center-section">
             <Link to="/">PEARLBBA - handmade jewellery online</Link>
         </div>
 
-        <div className='right-section'>
-            <div className='right-section-links'>
+        <div className="right-section">
+            <div className="section-links">
                 <Link to="/">Homepage</Link>
             </div>
-            <div className='right-section-links'>
+            <div className="section-links">
                 <Link to="/">About</Link>
              </div>
-             <div className='right-section-links'>
+             <div className="section-links">
                 <Link to="/">Contact us</Link>
             </div>
-            <div className="right-section-icons">
-              <PersonOutlineTwoToneIcon/>
-              <ShoppingCartTwoToneIcon/>
-              <FavoriteTwoToneIcon/>
+            <div className="section-icons">
               <SearchTwoToneIcon/>
+              <PersonOutlineTwoToneIcon/>
+              <FavoriteTwoToneIcon/>
+            <div className="cart-icon">
+              <ShoppingCartTwoToneIcon/>
+              <span>0</span>
+            </div>
             </div>
         </div>
       </div>
     </div>
+    
   )
 }
 
