51ba716a282e16a2d1bb09c13f590086530424ca
Anna M Attehed <anna.attehed@medieinstitutet.se>
2023-01-19 21:31:32 +0100

    added funtionality to make slider dynamic

--- client-site/pearlbba/src/components/Slider/Slider.jsx
+++ client-site/pearlbba/src/components/Slider/Slider.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect, setActive } from 'react';
 import './Slider.scss';
 import KeyboardDoubleArrowLeftTwoToneIcon from '@mui/icons-material/KeyboardDoubleArrowLeftTwoTone';
 import KeyboardDoubleArrowRightTwoToneIcon from '@mui/icons-material/KeyboardDoubleArrowRightTwoTone';
+
+
+
+const Slider = () => {
+
+const [currentSlide, setCurrentSlide] = useState(0);
+
 
 const data = [
     "https://images.pexels.com/photos/1456291/pexels-photo-1456291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
@@ -10,17 +17,23 @@ const data = [
     
 ];
 
-const Slider = () => {
+const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1 )
+};
+const nextSlide = () => {
+    setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1 )
+}
+
   return (
     <div className='slider'>
-        <div className="slider-container">
+        <div className="slider-container" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
             <img src={data[0]} alt="" />
             <img src={data[1]} alt="" />
             <img src={data[2]} alt="" />
         </div>
         <div className='icons'>
-            <div className="icon"> <KeyboardDoubleArrowLeftTwoToneIcon/> </div>
-            <div className="icon"> <KeyboardDoubleArrowRightTwoToneIcon/> </div>
+            <div className="icon" onClick={prevSlide}> <KeyboardDoubleArrowLeftTwoToneIcon/> </div>
+            <div className="icon" onClick={nextSlide}> <KeyboardDoubleArrowRightTwoToneIcon/> </div>
         </div>
 
     </div>
