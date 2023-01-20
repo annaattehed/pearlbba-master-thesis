33716b94a5782e33e79f69fc451b75025be8ed84
Anna M Attehed <anna.attehed@medieinstitutet.se>
2023-01-20 23:00:01 +0100

    condition added to add text to newest products

--- client-site/pearlbba/src/components/Imagecards/Imagecards.jsx
+++ client-site/pearlbba/src/components/Imagecards/Imagecards.jsx
@@ -7,11 +7,12 @@ const Imagecards = ({item}) => {
     <Link to={`/product/${item.id}`}>
     <div className='imagecards'>
         <div className='image'>
+            {item.isNew && <span>This just in!</span>}
             <img src="{item.img}" alt="" className="image" />
         </div>
         <h2>{item.title}</h2>
         <div className="prices">
-            <h3>${item.oldPrice}</h3>
+            {/* <h3>${item.oldPrice}</h3> */}
             <h3>${item.price}</h3>
         </div>
     </div>
