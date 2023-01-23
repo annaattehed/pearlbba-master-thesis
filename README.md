091886bf0fc810e75941001bc503d6f455a08673
Anna M Attehed <anna.attehed@medieinstitutet.se>
2023-01-23 17:54:23 +0100

    added content to single product page

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 The store is called Pearlbba and is a site builded to sell handcrafted jewellery
 
-# The store is builded on React for frontend and the headless CMS Strapi for backend
+# The store is builded with React for frontend and the headless CMS Strapi for backend
 
 Package manager: NPM (npm start to get the project up and runnning)
--- client-site/pearlbba/src/pages/Product-single/Product.jsx
+++ client-site/pearlbba/src/pages/Product-single/Product.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 import './Product.scss';
 
 const Product = () => {
+
+  const images = [
+    "https://images.pexels.com/photos/1456291/pexels-photo-1456291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/1268874/pexels-photo-1268874.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  ]
   return (
-    <div className='product-single'>Product</div>
+    <div className='product-single'>
+      <div className="productpage-left-section">
+        <div className="images">
+          <img src={images[0]} alt="" />
+          <img src={images[1]} alt="" />
+        </div>
+      </div>
+      <div className="productpage-right-section">
+        
+      </div>
+    </div>
   )
 }
 
