11fe2d769e112bc89e65220504358481bdd826c3
Anna M Attehed <anna.attehed@medieinstitutet.se>
2023-01-23 12:05:46 +0100

    added some styling to category component

--- client-site/pearlbba/src/components/Categories/Categories.jsx
+++ client-site/pearlbba/src/components/Categories/Categories.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Categories.scss';
 
 const Categories = () => {
   return (
     <div className='categories'>
         <div className="col">
-            <div className="row">row1</div>
-            <div className="row">row2</div>
+            <div className="row">
+                <img src="https://images.pexels.com/photos/3556665/pexels-photo-3556665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                <button>
+                    <Link className='link' to="/products/1">Bracelets</Link>
+                </button>
+            </div>
         </div>
         <div className="col">
-            <div className="row">r3</div>
+            <div className="row">
+            <img src="https://images.pexels.com/photos/3556665/pexels-photo-3556665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                <button>
+                    <Link className='link' to="/products/1">Necklaces</Link>
+                </button>
+            </div>
         </div>
         <div className="col col-1">
             <div className="row">
                 <div className="col">
-                    <div className="row">row4</div>
+                    <div className="row">
+                    <img src="https://images.pexels.com/photos/3556665/pexels-photo-3556665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                <button>
+                    <Link className='link' to="/products/1">Sale</Link>
+                </button>
+                    </div>
                 </div>
                 <div className="col">
-                    <div className="row">row5</div>
+                    <div className="row">
+                    <img src="https://images.pexels.com/photos/3556665/pexels-photo-3556665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                <button>
+                    <Link className='link' to="/products/1">New arrivals</Link>
+                </button>
+                    </div>
                 </div>
             </div>
-            <div className="row">row6</div>
         </div>
     </div>
   )
